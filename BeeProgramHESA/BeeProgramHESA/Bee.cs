9b53fefff25d1549d56f3f199b662f9179fc57de
Daniel OHanlon <daniel.ohanlon@iress.co.uk>
2020-07-01 12:37:56 +0100

    First commit

---
+++ BeeProgramHESA/BeeProgramHESA/Bee.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeeProgramHESA
+{
+    class Bee
+    {
+        public string name { get; set; }
+
+        public int health { get; set; }
+
+        public override string ToString()
+        {
+            return name + health.ToString();
+        }
+
+        //Bee Constructor
+        public Bee(string _name)
+        {
+            this.name = _name;
+            
+            this.health = 100;
+        }       
+
+
+        //Bee Taking Damage
+        public Bee damage(Bee bee, int damage)
+        {
+            //Subtract health first before checking if bee is dead            
+            bee.health = bee.health - damage;
+
+            //while the bee is alive subtract damage
+            while (!isDead(bee))
+            {               
+                return bee;
+            }
+            
+            bee.health = 0;           
+
+            //if dead just return bee
+            return bee;
+
+        }
+
+
+        //Check if bee is alive or dead
+        public bool isDead(Bee bee)
+        {
+            if (bee.name == "Queen")
+            {
+                if (bee.health <= 20)
+                {
+                    return true;
+                }
+            }
+            else if (bee.name == "Worker")
+            {
+                if (bee.health <= 70)
+                {
+                    return true;
+                }
+            }
+            else if (bee.name == "Drone")
+            {
+                if (bee.health <= 50)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+
+        
+
+
+    }
+
+}
+
---
+++ BeeProgramHESA/BeeProgramHESA/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BeeProgramHESA
+{
+    public partial class Form1 : Form
+    {
+
+        public Random random;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            Bee Worker = new Bee("Worker");            
+            Bee Queen = new Bee("Queen");           
+            Bee Drone = new Bee("Drone");
+
+            listBox1.Items.Add(Worker);
+            listBox1.Items.Add(Queen);
+            listBox1.Items.Add(Drone);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+       
+        }
+
+       
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int amount = 15;
+
+            Bee bee = (Bee)listBox1.SelectedItem;
+
+            if(bee != null)
+            {
+                Bee damagedBee = bee.damage(bee, amount);
+
+                if(damagedBee.health == 0)
+                {
+                    listBox2.Items.Add(damagedBee.name + " is dead");
+                }
+                else
+                {
+                    String currentBeeHealth = damagedBee.name + damagedBee.health.ToString();
+                    listBox2.Items.Add(currentBeeHealth);
+                }    
+            }
+        } 
+    }
+}
